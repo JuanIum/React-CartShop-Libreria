02a6750ae9a133ff1cc80bf569f2d6c0d370cc8f
Juan Martín Iummato <juaniummato@gmail.com>
2022-01-06 19:13:40 -0300

    2do cambio

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar(){
+
+    return (
+        <>
+            <ul>
+                <li>HOME</li>
+                <li>PRODUCTOS</li>
+                <li>CARRITO DE COMPRAS</li>
+                <li>NOSOTROS</li>
+                <li>CONTACTENOS</li>
+            </ul>
+        </>
+
+   )
+
+}
